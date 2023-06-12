6842cd9e0680468a34165d05c013658e19449fe8
Garci8 <nevred84@gmail.com>
2023-06-13 01:50:53 +0200

    prova

--- Entrega.java
+++ Entrega.java
@@ -59,7 +59,7 @@ class Entrega {
    */
   static class Tema1 {
     /*
-     * És cert que ∀x ∃!y. P(x) -> Q(x,y) ? no
+     * És cert que ∀x ∃!y. P(x) -> Q(x,y) ?
      */
     static boolean exercici1(int[] universe, Predicate<Integer> p, BiPredicate<Integer, Integer> q) {
       if(universe.length==0){ //univers buit, quant. universal cert
